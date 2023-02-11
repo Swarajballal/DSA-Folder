4cff96869e9a0b7f946cd7ec46f432b3303a39f4
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-02-11 11:52:33 +0530

    C++ Week 2

--- Week-2-CMN-functions/decimaltobinary.cpp
+++ Week-2-CMN-functions/decimaltobinary.cpp
@@ -5,16 +5,27 @@ using namespace std;
 int decimaltobinary(int num)
 {
     //Division method
+    // int binary = 0;
+    // int i = 0;
+    // while(num > 0)
+    // {
+    //     int bit = num%2;            //remainder but bits in reverse order
+    //     binary = bit*pow(10,i++)+binary;  //to reverse the bits we got to get an binary number eqivalent.
+    //     num = num/2;    //next iteration num should be num/2 
+    // }
+    // return binary;
+
+    //bitwise method 
+    //more efficent method
     int binary = 0;
-    int i = 0;
+    int i=0;
     while(num > 0)
     {
-        int bit = num%2;            //remainder but bits in reverse order
-        binary = bit*pow(10,i++)+binary;  //to reverse the bits we got to get an binary number eqivalent.
-        num = num/2;    //next iteration num should be num/2 
+        int bit = (num & 1);  //bitwise and with 1( we will get first bit)
+        binary += bit * pow(10, i++);
+        num = num >> 1;  //right shift by 1 bit  (so we get the nexy bit bu right shift right most bit that is the first bit will be ignored)
     }
     return binary;
-
 }
 
 int main()
@@ -23,6 +34,10 @@ int main()
     cout<<"Enter the number you want to convert to binary: ";
     cin >> num;
     int binary = decimaltobinary(num);
-    cout<<binary;
+    cout<<binary<<endl;
 }
+
+
+
+
 
