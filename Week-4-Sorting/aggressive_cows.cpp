1c597225cadadd24ec4fd56bc24d8a6959a2442e
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-04-06 00:18:40 +0530

    C++ Week 4

---
+++ Week-4-Sorting/aggressive_cows.cpp
@@ -0,0 +1,54 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPossible(vector<int> &stalls, int k, int sol) // to check if it is possible to place k cows with minimum distance sol
+{
+    int c = 1;           // to count the number of cows placed starting from the first cow
+    int pos = stalls[0]; // first cow is placed at the first stall
+    for (int i = 1; i < stalls.size(); i++)
+    {
+        if (stalls[i] - pos >= sol) // if the distance between the current stall and the last placed cow is greater than or equal to the minimum distance then place the cow at the current stall
+        {
+            c++;             // increase the count of cows placed
+            pos = stalls[i]; // update the position of the last placed cow
+        }
+        if (c == k)
+            return true; // if the number of cows placed is equal to k then return true
+    }
+    return false; // if the number of cows placed is less than k then return false
+}
+
+int solve(int n, int k, vector<int> &stalls)
+{
+
+    sort(stalls.begin(), stalls.end());                         // if not sorted then sort it
+    int start = 0, end = stalls[stalls.size() - 1] - stalls[0]; // to get a range for minimum distance possible so start = 0 and end = last element - first element
+    int ans = -1;                                               // to store the answer
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (isPossible(stalls, k, mid)) // if it is possible to place k cows with minimum distance mid then we will try to increase the distance
+        {
+            ans = mid;       // store the answer
+            start = mid + 1; // increase the distance see if we can get a better answer since we want the maximum possible distance
+        }
+        else
+        {
+            end = mid - 1; // if it is not possible to place k cows with minimum distance mid then we will try to decrease the distance
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    // vector<int> stalls = {1, 2, 4, 8, 9};
+    vector<int> stalls = {10, 1, 2, 7, 5};
+    int n = stalls.size();
+    int k = 3;
+    int ans = solve(n, k, stalls);
+    cout << "The Minimum distance between any two of them is the maximum possible: " << ans << endl;
+    return 0;
+}
