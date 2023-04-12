4e968f211edf1e999d941e161b6401a4940c1173
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-04-12 21:57:26 +0530

    C++ Week 5

--- Week-5-Char-array&Strings/reverseAString.cpp
+++ Week-5-Char-array&Strings/reverseAString.cpp
@@ -1,19 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+int lengthstring(char name[])
+{
+    int i = 0;              // i is the index of the string
+    while (name[i] != '\0') // if character is not null
+    {
+        i++; // increment the index
+    }
+    return i; // return the length of the string
+}
+
+// two pointer approach
+
+void reversestring(char name[])
+{
+    int start = 0;
+    int end = lengthstring(name) - 1;
+    // or int end = strlen(name) - 1;  // pre-defined function
+    while (start <= end)
+    {
+        swap(name[start], name[end]); // swap the characters
+        start++;                      // increment the start index
+        end--;                        // decrement the end index
+    }
+}
+
 int main()
 {
-    char name[100];
-    cout<<"Enter a name: ";
+    char name[100]; // 100 is the maximum length of the string
+    cout << "Enter a name: ";
     cin >> name;
-    int start = 0;
-    int end = strlen(name)-1;
-    while(start <= end)
-    {
-        swap(name[start], name[end]);
-        start++;
-        end--;
-    }
-    for(int i = 0; i < strlen(name); i++)
+    reversestring(name);
+    for (int i = 0; i < strlen(name); i++)
         cout << name[i];
     return 0;
 }
