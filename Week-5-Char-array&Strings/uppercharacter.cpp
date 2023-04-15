05c8870f4e93b322b2029e0042e77d7fb2a5aa56
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2023-04-15 20:04:54 +0530

    C++ Week 5

---
+++ Week-5-Char-array&Strings/uppercharacter.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int stringlength(char name[])
+{
+    int i = 0;
+    int len = 0;
+    while (name[i] != '\0')
+    {
+        i++;
+        len++;
+    }
+    return len;
+}
+
+void uppercasechar(char name[])
+{
+    int length = stringlength(name);
+    for (int i = 0; i < length; i++)
+    {
+        name[i] = name[i] - 'a' + 'A';
+    }
+
+    cout << "The name in uppercase is: ";
+    for (int i = 0; i < length; i++)
+    {
+        cout << name[i];
+    }
+}
+
+int main()
+{
+    char name[100];
+    cout << "Enter the name in small letters: ";
+    cin.getline(name, 100);
+    uppercasechar(name);
+    return 0;
+}
