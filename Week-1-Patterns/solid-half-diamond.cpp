47d8b96b0e4d2815bf91b3f49bd46621a95f40a2
Swaraj Ballal <swaraj.ballal@vit.edu.in>
2024-01-15 22:41:03 +0530

    solid half diamond

--- Week-1-Patterns/solid-half-diamond.cpp
+++ Week-1-Patterns/solid-half-diamond.cpp
@@ -3,23 +3,41 @@ using namespace std;
 
 int main()
 {
-    int row,col,rowscount;
-    cout<<"Enter the numner of row: ";
-    cin >> rowscount;
-    for(row=0; row<rowscount; row++)
-    {
-        for(col=0; col<row+1; col++)    //normal half soild pyramid
-        {
-            cout<<"*";
+//    int row,col,rowscount;
+//    cout<<"Enter the numner of row: ";
+//    cin >> rowscount;
+//    for(row=0; row<rowscount; row++)
+//    {
+//        for(col=0; col<row+1; col++)    //normal half soild pyramid
+//        {
+//            cout<<"*";
+//        }
+//        cout<<endl;
+//    }
+//     for(row=0; row<rowscount; row++)
+//    {
+//        for(col=0; col<rowscount-row-1; col++)  //normal inverted solid half pyramid
+//        {
+//            cout<<"*";
+//        }
+//        cout<<endl;
+//    }
+
+    int rows;
+    cout<<"Enter the number of rows: ";
+    cin >> rows;
+
+    for(int row = 0; row < 2*rows - 1; row++){
+        int cond = 0;
+        if(row < rows) {
+            cond = row;
+        }else{
+            cond = rows - (row % rows) - 2;
         }
-        cout<<endl;
-    }
-     for(row=0; row<rowscount; row++)
-    {
-        for(col=0; col<rowscount-row-1; col++)  //normal inverted solid half pyramid
-        {
-            cout<<"*";
+        for(int col = 0; col <= cond; col++){
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
+    return 0;
 }
